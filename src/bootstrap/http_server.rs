0b3a63e8cef3d578381f9b31bd824f00a4c5a693
Abner Kaizer <mail@abnerkaizer.com>
2026-02-12 17:13:55 -0300

    feat: added CORS

--- src/bootstrap/http_server.rs
+++ src/bootstrap/http_server.rs
@@ -1,5 +1,6 @@
+use actix_cors::Cors;
 use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer, http::header, web};
 
 use crate::{
     AppState, auth::middleware::AuthMiddleware, config::AppConfig, controller::auth_controller,
@@ -16,6 +17,15 @@ pub async fn start_http_server(
 
     HttpServer::new(move || {
         App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:8081")
+                    .allowed_methods(vec!["GET", "POST", "PUT", "PATCH", "DELETE"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .supports_credentials()
+                    .max_age(3600),
+            )
             .wrap(Logger::default())
             .app_data(app_state.clone())
             .service(
